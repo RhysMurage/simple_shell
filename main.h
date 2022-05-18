04e6505e055444f7296cd188b36d5193a75bd46c
RhysMurage <rhysmwangi96@gmail.com>
2022-05-18 10:14:31 -0400

    Add prototypes

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -6,9 +5,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv);
 
 int _putchar(char c);
+
 int _strlen(char *s);
 
-int getInput();
+unsigned int _strspn(char *s, char *accept);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int execute(char *argv[]);
+
+char *readline(void);
+
+char **tokenize(char *line);
 #endif /* _MAIN_H */
