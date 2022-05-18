f8dc60fb7f7da0b6cdaf730da71445d77b97f417
RhysMurage <rhysmwangi96@gmail.com>
2022-05-18 12:43:09 -0400

    Remove unused variables

--- shell.c
+++ shell.c
@@ -2,18 +2,17 @@
 
 /**
   * main- function that displays prompt and reads input from stdin
-  * @argc: integer variable for number of arguments passed
+  * @: integer variable for number of arguments passed
   * @argv: pointer to command arguments
   *
   * Return: Always (0);
   */
 
-int main(int argc, char **argv)
+int main()
 {
 	int status = 1;
 	char *line;
 	char **tokens;
-	char *delimiters = " :'\n''\t'";
 
 	while (status)
 	{
